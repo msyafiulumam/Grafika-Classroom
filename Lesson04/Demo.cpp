9250f24d31bf28bff38f900452d834b33d655c59
M Syafiul Umam <msyafiulumam@student.ub.ac.id>
2020-12-19 19:08:34 +0700

    new texture

--- Lesson04/Demo.cpp
+++ Lesson04/Demo.cpp
@@ -261,6 +261,28 @@ void Demo::DrawColoredFrame()
 	glBindVertexArray(0);
 }
 
+void Demo::BuildColoredStand()
+{
+	glGenTextures(1, &texture3);
+	glBindTexture(GL_TEXTURE_2D, texture3);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	int width, height;
+	unsigned char* image = SOIL_load_image("frame.png", &width, &height, 0, SOIL_LOAD_RGBA);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+	SOIL_free_image_data(image);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	float vertices[] = {
+		
+	};
+
+}
+
+void Demo::DrawColoredStand()
+{
+}
+
 void Demo::BuildColoredPlane()
 {
 	// Load and create a texture 
--- Lesson04/Demo.h
+++ Lesson04/Demo.h
@@ -14,6 +14,7 @@ public:
 	~Demo();
 private:
 	GLuint shaderProgram, VBO, VAO, EBO, texture, VBO2, VAO2, EBO2, texture2;
+	GLuint VBO3, VAO3, EBO3, texture3;
 	float viewCamX, viewCamY, viewCamZ, upCamX, upCamY, upCamZ, posCamX, posCamY, posCamZ, CAMERA_SPEED, fovy;
 	virtual void Init();
 	virtual void DeInit();
@@ -31,6 +32,8 @@ private:
 	void FlyCamera(float speed);
 	void BuildColoredFrame();
 	void DrawColoredFrame();
+	void BuildColoredStand();
+	void DrawColoredStand();
 
 };
 
